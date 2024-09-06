8de115cb905fb1604c890d1ce186aa468d124128
jasminakter7 <jasminjui0777@gmail.com>
2024-09-06 17:05:55 +0600

    MobileNav init

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = ({ user }: SiderbarProps ) => {
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
-            <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+            <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
                 <Image src="/icons/logo.svg"
                 width={34}
                 height={34}
@@ -41,7 +41,11 @@ const Sidebar = ({ user }: SiderbarProps ) => {
                     </Link>
                 )
             })}
+
+            USER
         </nav>
+
+        FOOTER
     </section>
   )
 }
