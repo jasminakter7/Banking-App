98ee9aa7cd5864526c7cab173e95f59232d6ef2b
jasminakter7 <jasminjui0777@gmail.com>
2024-09-17 17:24:52 +0600

    card design onging

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -8,13 +8,27 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
         <Link href="/" className="bank-card">
             <div className="bank-card_content">
                 <div>
-                    <h1 className="text-16 fontsemibold text-white">
+                    <h1 className="text-16 font-semibold text-white">
                         {account?.name || userName}
                     </h1>
                     <p className="font-ibm-plex-serif font-black text-white">
                         {formatAmount(account?.currentBalance)}
                     </p>
                 </div>
+
+                <article className="flex flex-col gap-2">
+                    <div className="flex justify-between">
+                        <h1 className="text-12 font-semibold text-white">
+                            {userName}
+                        </h1>
+                        <h2 className="text-12 font-semibold text-white">
+                            ** / **
+                        </h2>
+                    </div>
+                    <p className="text-14 font-semibold tracking-[1.1px] text-white">
+                        **** **** **** <span className="text-16">{account?.mask}</span>
+                    </p>
+                </article>
             </div>
         </Link>
     </div>
@@ -22,3 +36,4 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
 }
 
 export default BankCard
+
