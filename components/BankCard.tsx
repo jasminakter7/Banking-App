24ddd7252c3144df7be629c9f5a16992c12d77dc
jasminakter7 <jasminjui0777@gmail.com>
2024-09-17 15:52:56 +0600

    bank card design init

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,8 +1,23 @@
+import { formatAmount } from '@/lib/utils'
+import Link from 'next/link'
 import React from 'react'
 
-const BankCard = () => {
+const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
   return (
-    <div>BankCard</div>
+    <div className="flex flex-col">
+        <Link href="/" className="bank-card">
+            <div className="bank-card_content">
+                <div>
+                    <h1 className="text-16 fontsemibold text-white">
+                        {account?.name || userName}
+                    </h1>
+                    <p className="font-ibm-plex-serif font-black text-white">
+                        {formatAmount(account?.currentBalance)}
+                    </p>
+                </div>
+            </div>
+        </Link>
+    </div>
   )
 }
 
